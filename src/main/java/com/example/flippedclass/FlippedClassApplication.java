3fdb7eb3b3cb204c5ec7da29fda479131f6a24bb
Nguyen Quang Huy <huynqhe182121@fpt.edu.vn>
2026-05-13 18:39:46 +0700

    first commit

---
+++ src/main/java/com/example/flippedclass/FlippedClassApplication.java
@@ -0,0 +1,13 @@
+package com.example.flippedclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlippedClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlippedClassApplication.class, args);
+    }
+
+}
