db065d2c2039ff138d959e421529f3f0fe232b74
Nguyenhuy <huynqhe182121@fpt.edu.vn>
2026-05-19 15:41:59 +0700

    change pass

--- src/main/java/com/example/flippedclass/util/ValidateChangePass.java
+++ src/main/java/com/example/flippedclass/util/ValidateChangePass.java
@@ -1,22 +1,26 @@
 package com.example.flippedclass.util;
 import com.example.flippedclass.dto.ChangePasswordRequest;
-
+import org.springframework.stereotype.Component;
+@Component
 public class ValidateChangePass {
-    public void validatePassWord(ChangePasswordRequest request){
-        if(request == null){
-            throw new IllegalArgumentException("Requert body must not be null");
+
+    public void validatePassWord(ChangePasswordRequest request) {
+        if (request == null) {
+            throw new IllegalArgumentException("Request body must not be null");
         }
-        if(request.getOldPassword().trim().isEmpty() || request.getOldPassword() == null){
+
+
+        if (request.getOldPassword() == null || request.getOldPassword().trim().isEmpty()) {
             throw new IllegalArgumentException("Old password must not be blank");
         }
-        if(request.getNewPassWord().trim().isEmpty() || request.getNewPassWord() == null){
+        if (request.getNewPassWord() == null || request.getNewPassWord().trim().isEmpty()) {
             throw new IllegalArgumentException("New password must not be blank!");
         }
-        if(request.getConfirmPassword().trim().isEmpty() || request.getConfirmPassword() == null){
+        if (request.getConfirmPassword() == null || request.getConfirmPassword().trim().isEmpty()) {
             throw new IllegalArgumentException("Confirm password must not be blank!");
         }
 
-        if(!request.getNewPassWord().equals(request.getNewPassWord())){
+        if (!request.getNewPassWord().equals(request.getConfirmPassword())) {
             throw new IllegalArgumentException("Error: New password and confirm password do not match!");
         }
     }
