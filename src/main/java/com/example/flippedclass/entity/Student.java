4cdf9de29cbc790139bd34c2ffdc91c3a7296ec2
Thanh2151555 <ngovthanh0404@gmail.com>
2026-06-11 17:23:17 +0700

    Update student dashboard before merge

--- src/main/java/com/example/flippedclass/entity/Student.java
+++ src/main/java/com/example/flippedclass/entity/Student.java
@@ -1,11 +1,9 @@
 package com.example.flippedclass.entity;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
 
 public class Student {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
