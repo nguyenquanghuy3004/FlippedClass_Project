b2012322c7f794492d8f6b7b69fe61e46ed6c0fd
zezinguyen <dongsuper280@gmail.com>
2026-07-05 21:41:32 +0700

    sửa lại giao diện supporter

--- src/main/java/com/example/flippedclass/controller/LearningPathController.java
+++ src/main/java/com/example/flippedclass/controller/LearningPathController.java
@@ -83,14 +83,14 @@ public class LearningPathController {
     }
 
 
-    @PreAuthorize("@spaceSecurity.hasRoleInSpace(#spaceId, 'OWNER')")
+    @PreAuthorize("@spaceSecurity.hasRoleInSpace(#spaceId, 'OWNER', 'SUPPORTER')")
     @DeleteMapping("/{pathId}")
     public ResponseEntity<?> delete(@PathVariable Long spaceId, @PathVariable Long pathId) {
         learningPathService.deleteLearningPathModul(spaceId, pathId);
         return ResponseEntity.ok(new MessageResponse("Xóa module thành công"));
     }
 
-    @PreAuthorize("@spaceSecurity.hasRoleInSpace(#spaceId, 'OWNER')")
+    @PreAuthorize("@spaceSecurity.hasRoleInSpace(#spaceId, 'OWNER', 'SUPPORTER')")
     @DeleteMapping
     public ResponseEntity<?> deleteAll(@PathVariable Long spaceId) {
         learningPathService.deleteAllLearningPaths(spaceId);
