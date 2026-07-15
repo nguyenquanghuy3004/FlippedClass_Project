e88516394e67dcb20cdaf7b8904d449226f4f9a7
zezinguyen <dongsuper280@gmail.com>
2026-07-16 00:04:30 +0700

    sửa lại giao diện đánh giá bên student

--- src/main/java/com/example/flippedclass/service/impl/EvaluationServiceImpl.java
+++ src/main/java/com/example/flippedclass/service/impl/EvaluationServiceImpl.java
@@ -97,7 +97,7 @@ public class EvaluationServiceImpl implements EvaluationService {
         User student = UserServiceImpl.findUser(userRepository, studentId);
 
         List<InteractionLogResponse> history = interactionLogRepository
-                .findByStudentIdAndLearningPathIdOrderByOccurredAtDesc(studentId, session.getLearningPath().getId())
+                .findTop20ByStudentIdAndLearningPath_LearningSpace_IdOrderByOccurredAtDesc(studentId, session.getLearningPath().getLearningSpace().getId())
                 .stream()
                 .map(this::toInteractionResponse)
                 .toList();
